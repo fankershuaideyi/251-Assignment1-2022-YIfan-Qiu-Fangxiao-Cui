5ecd0b24947c277c0ff759bf82048a8127c5d280
Cuifangxiao <1584797122@qq.com>
2022-10-02 17:14:14 +0800

    10.2 vesion1.1

--- src/main/java/RunHere.java
+++ src/main/java/RunHere.java
@@ -1,12 +1,13 @@
 import java.awt.*;
 
 public class RunHere {
-
+   public static int width;
+   public static int height ;
     public static void main(String[] args) {
         Toolkit tit = Toolkit.getDefaultToolkit();
         Dimension screenSize = tit.getScreenSize();
-        final int width = screenSize.width;
-        final int height = screenSize.height;
+        width = screenSize.width;
+        height = screenSize.height;
         Window window = new Window(width,height);
     }
 }
--- src/main/java/Window.java
+++ src/main/java/Window.java
@@ -1,17 +1,18 @@
 import javax.swing.*;
-
+import java.awt.*;
 
 public class Window extends JFrame {
     JFrame jf = new JFrame();
-
+    JTabbedPane tabbedPane = new JTabbedPane();
+    int textAreaIndex = 1;
     Window(int w,int h){
         init(w,h);
         jf.setVisible(true);
-        jf.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        jf.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+
+
     }
     void init(int w,int h){
-
-
         //设置主页面
         jf.setTitle("Text Editor");
         jf.setBounds((w-500)/2,(h-700)/2,500,700);
@@ -19,7 +20,21 @@ public class Window extends JFrame {
         //初始化菜单
         initMenuBar();
 
+        //初始化页签
+        JTextPane workArea = new JTextPane();
+        JScrollPane scrollPane = new JScrollPane(workArea);
 
+        //将字体变蓝
+        workArea.setForeground(Color.BLUE);
+
+        //字体大小变粗等
+        workArea.setFont(new Font("eag fawdawrawr awr aw rawr ", Font.BOLD, 20));
+
+        //加入到主页面中
+        jf.add(scrollPane);
+
+//        initJTabbedPane(tabbedPane);
+//        jf.getContentPane().add(tabbedPane);
     }
     void initMenuBar(){
         JMenuBar menuBar = new JMenuBar();
@@ -48,7 +63,7 @@ public class Window extends JFrame {
         menu_file.add(fileItem_exit);
 
         //Search 菜单
-        JMenuItem searchItem_ = new JMenuItem();
+
 
 
         //View 菜单
@@ -60,5 +75,46 @@ public class Window extends JFrame {
         JMenuItem helpItem_about = new JMenuItem("about");
         menu_help.add(helpItem_about);
 
+        //new
+        fileItem_new.addActionListener(e -> addNewTabbedPane());
     }
+
+    void initJTabbedPane(JTabbedPane tabbedPane) {
+        //新标签的title
+        String title;
+        if (textAreaIndex == 1) {
+            title = "文本文档.txt";
+        } else {
+            title = "文本文档" + (textAreaIndex) + ".txt";
+        }
+
+        //创建workArea
+        JTextPane workArea = new JTextPane();
+        JScrollPane scrollPane = new JScrollPane(workArea);
+
+
+        //加到标签栏中
+        tabbedPane.addTab(title, null, scrollPane, null);
+
+        //获取当前选项卡的位置
+        int tabCount = tabbedPane.getTabCount();
+
+        //将选项卡切换到新创建的一页上
+        tabbedPane.setSelectedIndex(tabCount - 1);
+
+        //实现关闭选项卡的功能通过ButtonTabComponent类
+        tabbedPane.setTabComponentAt(tabCount-1,new ButtonTabComponent(tabbedPane));
+
+        //页签的页码加1
+        textAreaIndex++;
+    }
+
+    //创建一个新的标签
+    void addNewTabbedPane(){
+//        initJTabbedPane(tabbedPane);
+        RunHere runHere = new RunHere();
+
+        Window window1 = new Window(RunHere.width+200, RunHere.height+100);
+    }
+
 }

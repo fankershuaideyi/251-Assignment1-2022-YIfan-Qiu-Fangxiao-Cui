2dad99b0caebe74208cabbce76b8e8ef85a268f3
Cuifangxiao <1584797122@qq.com>
2022-10-03 18:35:30 +0800

    podafang

--- src/main/java/RunHere.java
+++ src/main/java/RunHere.java
@@ -1,13 +1,13 @@
 import java.awt.*;
 
 public class RunHere {
-   public static int width;
-   public static int height ;
+    public static int width;
+    public static int height ;
     public static void main(String[] args) {
         Toolkit tit = Toolkit.getDefaultToolkit();
         Dimension screenSize = tit.getScreenSize();
         width = screenSize.width;
         height = screenSize.height;
-        Window window = new Window(width,height);
+        Window window = new Window();
     }
 }
--- src/main/java/Window.java
+++ src/main/java/Window.java
@@ -1,52 +1,58 @@
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.io.BufferedWriter;
+import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileWriter;
+import java.io.FileReader;
 import java.io.IOException;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
 
 import org.apache.pdfbox.pdmodel.PDDocument;
 import org.apache.pdfbox.pdmodel.PDPage;
-
 public class Window extends JFrame {
-    JFrame jf = new JFrame();
-    JTabbedPane tabbedPane = new JTabbedPane();
-    int textAreaIndex = 1;
-    Window(int w,int h){
-        init(w,h);
+
+    private static int width_screen = RunHere.width;
+    private static int height_screen = RunHere.height;
+    private JTextArea workArea;
+    private JScrollPane scrollPane;
+    private JFrame jf = new JFrame();
+
+
+    Window() {
+        init(width_screen, height_screen);
         jf.setVisible(true);
         jf.setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
-
-
     }
-    void init(int w,int h){
+
+    void init(int w, int h) {
         //设置主页面
         jf.setTitle("Text Editor");
-        jf.setBounds((w-500)/2,(h-700)/2,500,700);
+        jf.setBounds((w - 500) / 2, (h - 700) / 2, 500, 700);
 
-        JTextPane workArea = new JTextPane();
-        JScrollPane scrollPane = new JScrollPane(workArea);
+        width_screen += 200;
+        height_screen += 100;
+
+        //初始化页签
+        workArea = new JTextArea();
+        scrollPane = new JScrollPane(workArea);
+
+        jf.add(scrollPane);
+
         //初始化菜单
         initMenuBar(workArea);
 
-        //初始化页签
-
-
         //将字体变蓝
-        workArea.setForeground(Color.BLUE);
+//        workArea.setForeground(Color.BLUE);
 
         //字体大小变粗等
-        workArea.setFont(new Font("eag fawdawrawr awr aw rawr ", Font.BOLD, 20));
-
-        //加入到主页面中
-        jf.add(scrollPane);
-
-//        initJTabbedPane(tabbedPane);
-//        jf.getContentPane().add(tabbedPane);
+//        workArea.setFont(new Font("Cui", Font.BOLD, 20));
     }
-    void initMenuBar(JTextPane workArea){
+
+
+
+
+
+    void initMenuBar(JTextArea workArea){
         JMenuBar menuBar = new JMenuBar();
         jf.setJMenuBar(menuBar);
 
@@ -64,35 +70,34 @@ public class Window extends JFrame {
         JMenuItem fileItem_new = new JMenuItem("new");
         JMenuItem fileItem_open = new JMenuItem("open");
         JMenuItem fileItem_save = new JMenuItem("save");
+
+
         FileDialog saveDia;
         saveDia = new FileDialog(this,"save as(A)",FileDialog.SAVE);
         // a funtion for save
-        fileItem_save.addActionListener(new ActionListener(){
-            @Override
-            public void actionPerformed(ActionEvent e){
-                File fileS = null;
-                if(fileS == null){
-                    saveDia.setVisible(true);
-                    String dirPath = saveDia.getDirectory();
-                    String fileName = saveDia.getFile();
+        fileItem_save.addActionListener(e -> {
+            File fileS = null;
+            if(fileS == null){
+                saveDia.setVisible(true);
+                String dirPath = saveDia.getDirectory();
+                String fileName = saveDia.getFile();
 
-                    if(dirPath == null || fileName == null) {
-                        return;
-                    }
-
-                    fileS = new File(dirPath,fileName);
+                if(dirPath == null || fileName == null) {
+                    return;
                 }
+                fileS = new File(dirPath,fileName);
+            }
 
-                try{
-                    BufferedWriter bufw = new BufferedWriter(new FileWriter(fileS));
-                    String text = workArea.getText();
-                    bufw.write(text);
-                    bufw.close();
-                }catch(IOException er){
-                    throw new RuntimeException("file saved failed");
-                }
+            try{
+                BufferedWriter bufw = new BufferedWriter(new FileWriter(fileS));
+                String text = workArea.getText();
+                bufw.write(text);
+                bufw.close();
+            }catch(IOException er){
+                throw new RuntimeException("file saved failed");
             }
         });
+
 
         JMenuItem fileItem_print = new JMenuItem("print");
         JMenuItem fileItem_exit = new JMenuItem("exit");
@@ -105,7 +110,6 @@ public class Window extends JFrame {
         //Search 菜单
 
 
-
         //View 菜单
         JMenuItem viewItem_TD = new JMenuItem("Time and Date");
         menu_view.add(viewItem_TD);
@@ -116,46 +120,49 @@ public class Window extends JFrame {
         menu_help.add(helpItem_about);
 
         //new
-        fileItem_new.addActionListener(e -> addNewTabbedPane());
+        fileItem_new.addActionListener(e -> New());
+
+        //open
+        fileItem_open.addActionListener(e -> openFile());
+
+        //about
+        helpItem_about.addActionListener(e -> about());
     }
 
-    void initJTabbedPane(JTabbedPane tabbedPane) {
-        //新标签的title
-        String title;
-        if (textAreaIndex == 1) {
-            title = "文本文档.txt";
-        } else {
-            title = "文本文档" + (textAreaIndex) + ".txt";
-        }
-
-        //创建workArea
-        JTextPane workArea = new JTextPane();
-        JScrollPane scrollPane = new JScrollPane(workArea);
-
-
-        //加到标签栏中
-        tabbedPane.addTab(title, null, scrollPane, null);
-
-        //获取当前选项卡的位置
-        int tabCount = tabbedPane.getTabCount();
-
-        //将选项卡切换到新创建的一页上
-        tabbedPane.setSelectedIndex(tabCount - 1);
-
-        //实现关闭选项卡的功能通过ButtonTabComponent类
-        tabbedPane.setTabComponentAt(tabCount-1,new ButtonTabComponent(tabbedPane));
-
-        //页签的页码加1
-        textAreaIndex++;
-    }
 
     //创建一个新的标签
-    void addNewTabbedPane(){
-//        initJTabbedPane(tabbedPane);
-        RunHere runHere = new RunHere();
-
-        Window window1 = new Window(RunHere.width+200, RunHere.height+100);
+    void New() {
+        new Window();
+        width_screen += 200;
+        height_screen += 100;
     }
+
+    void openFile() {
+        JFileChooser jFileChooser = new JFileChooser();
+        int chose = jFileChooser.showOpenDialog(null);
+        if (chose == JFileChooser.CANCEL_OPTION) {
+            return;
+        }
+        File F = jFileChooser.getSelectedFile();
+        if (F != null) {
+            jf.setTitle(F.getName());
+            try {
+                BufferedReader br = new BufferedReader(new FileReader(F));
+                String line;
+                while((line = br.readLine()) != null){
+                    workArea.append(line + "\r\n");
+                }
+            }catch (IOException er1){
+                throw new RuntimeException("Failed!！");
+            }
+        }
+    }
+
+    void about(){
+        JOptionPane.showMessageDialog(null,"Qiu Yifan\n" +
+                "Cui Fangxiao is handsome","About Us",JOptionPane.PLAIN_MESSAGE);
+    }
+
     void saveasPdf(){
         PDDocument document = new PDDocument();
         PDPage my_page=new PDPage();
